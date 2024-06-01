69d84afa66d9cc007451710ef3a1a3692a154625
duc-la <duclagithub@gmail.com>
2024-06-01 15:46:51 -0700

    Fix Alignment, fix fonts. Need to addd scroll features

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="app">
       <main>
-        <h1>Welcome to My Portfolio</h1>
+        <h1>Duc La</h1>
+        <h2>Software Engineer</h2>
+        <h3>I am interested in algorithms and data science</h3>
+        <div className="separator">
+          <ul>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
         {/* Add more components here */}
       </main>
-      <Header />
+      <header>
+      {/* <nav> */}
+        {/* <ul> */}
+          {/* <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li> */}
+        {/* </ul> */}
+      {/* </nav> */}
+    </header>
     </div>
   );
 };
